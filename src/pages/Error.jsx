7ad6df1637a0c00606f2a3acba3bd26c0bf31f96
VladoMilicic <vlado_milicic@hotmail.com>
2023-03-01 17:35:02 +0100

    added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { useNavigate, useRouteError } from 'react-router'
+import { Link } from 'react-router-dom'
+import { ArrowUturnLeftIcon, HomeIcon } from '@heroicons/react/24/solid'
 const Error = () => {
+    const error = useRouteError();
+    const navigate = useNavigate();
   return (
-    <div>Error</div>
+    <div className='error'>
+        <p>{error.message || error.statusText}</p>
+        <div className="flex-md">
+            <button className='btn btn--dark' onClick={()=>{navigate(-1)}}>
+                <ArrowUturnLeftIcon width={20} />
+            </button>
+            <Link to="/" className='btn btn--dark'>
+                <HomeIcon width={20} />
+                <span>Go Home</span>
+            </Link>
+        </div>
+    </div>
   )
 }
 
-export default Error
+export default Error;
